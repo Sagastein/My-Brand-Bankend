8b53b180b403f2970eb6a214503ea8b75cb65026
Sagacode <ishimwe.sage.3@gmail.com>
2024-02-29 16:54:13 +0200

    Refactor routes to include authentication and authorization middleware

--- src/routes/blog.route.ts
+++ src/routes/blog.route.ts
@@ -1,12 +1,25 @@
 import BlogController from "../controllers/blog.controller";
 import { Router } from "express";
 import { upload } from "../config/multer";
+import { checkAuth, checkAdmin } from "../middleware/auth.middleware";
 const router = Router();
 router.get("/", BlogController.getBlogs);
-router.post("/", upload.single("image"), BlogController.createBlog);
+router.post(
+  "/",
+  checkAuth,
+  checkAdmin,
+  upload.single("image"),
+  BlogController.createBlog
+);
 router.get("/:id", BlogController.getBlogById);
-router.delete("/:id", BlogController.deleteBlog);
-router.patch("/:id", upload.single("image"), BlogController.updateBlog);
-router.patch("/like/:id", BlogController.likeBlog);
+router.delete("/:id", checkAuth, checkAdmin, BlogController.deleteBlog);
+router.patch(
+  "/:id",
+  checkAuth,
+  checkAdmin,
+  upload.single("image"),
+  BlogController.updateBlog
+);
+router.patch("/like/:id", checkAuth, BlogController.likeBlog);
 
 export { router as blogRouter };
