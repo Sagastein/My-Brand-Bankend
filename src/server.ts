b1721343f34c8719e6b1d62748e9a0bf710f8152
Sagacode <ishimwe.sage.3@gmail.com>
2024-03-10 21:42:21 +0200

    (chore)update server app separate app configuration to app listening

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
+// src/server.ts
 import express, { Router } from "express";
-import connectDB from "./config/connectDB";
 import { createServer } from "http";
 import { UserRouter } from "./routes/user.route";
 import { MessageRouter } from "./routes/message.route";
@@ -10,28 +10,49 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
+import connectDB from "./config/connectDB";
+
 const swaggerDocument = YAML.load("./src/config/swagger.yaml");
-export const server = express();
-const PORT = 8000;
-export const testServer = createServer(server);
-server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-server.use(express.json());
-server.use(cors());
-server.use(bodyParser.json());
-server.use(cookieParser());
-server.use(express.urlencoded({ extended: true }));
-const app = Router();
-server.use("/api/v1", app);
-app.use("/users", UserRouter);
-app.use("/messages", MessageRouter);
-app.use("/blogs", blogRouter);
-app.use("/comments", commentRouter);
-server.get("/", (req, res) => {
-  res.send("Hello World");
-});
+
+export function configureApp(): express.Application {
+  const app = express();
+
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use(express.json());
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(cookieParser());
+  app.use(express.urlencoded({ extended: true }));
+
+  const apiRouter = Router();
+  apiRouter.use("/users", UserRouter);
+  apiRouter.use("/messages", MessageRouter);
+  apiRouter.use("/blogs", blogRouter);
+  apiRouter.use("/comments", commentRouter);
+
+  app.use("/api/v1", apiRouter);
+
+  app.get("/", (req, res) => {
+    res.status(200).send("welcome to sage brand");
+  });
+  app.all("*", (req, res) => {
+    res.status(404).json({
+      message: "Route not found",
+    });
+  });
+
+  return app;
+}
+//app.all route not found
+
+const app = configureApp();
+const PORT = process.env.PORT || 8000;
+const server = createServer(app);
 
 connectDB().then(() => {
   server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 });
+
+export { server };
