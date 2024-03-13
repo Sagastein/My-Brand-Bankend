59994ccdf8d7326f2537be82dfa681b7c83f6303
Sagacode <ishimwe.sage.3@gmail.com>
2024-03-13 12:26:21 +0200

    Update Jest configuration timeout and silent

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,14 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testTimeout: 30000,
+  testTimeout: 15000,
   verbose: true,
   forceExit: true,
   clearMocks: true,
   resetMocks: true,
   restoreMocks: true,
   clearMocks: true,
+  silent: true,
   collectCoverage: true,
   coverageReporters: ["lcov", "text"],
   collectCoverageFrom: [
