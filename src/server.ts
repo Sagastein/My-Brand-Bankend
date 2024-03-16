9584656811fb77448b11b3fd60e83e3365a4e42d
Sagacode <ishimwe.sage.3@gmail.com>
2024-03-16 23:54:08 +0200

    Update CORS origin in server.ts

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,11 @@ import connectDB from "./config/connectDB";
 
 const swaggerDocument = YAML.load("./src/config/swagger.yaml");
 const corsOption = {
-  origin:'*',
+  origin: [
+    "http://localhost:5173",
+    "http://127.0.0.1:5500",
+    "https://my-brand-bankend.onrender.com",
+  ],
   credentials: true,
 };
 export function configureApp(): express.Application {
